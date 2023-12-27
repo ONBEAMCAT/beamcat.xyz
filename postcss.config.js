572573387b7632e50a73a9687cbaa901e36889a8
Curtis Cummings <curtis@floornfts.io>
2023-12-27 12:00:06 -0500

    fine tuning

--- postcss.config.js
+++ postcss.config.js
@@ -3,11 +3,9 @@ const cssnano = require('cssnano');
 module.exports = {
   plugins: [
     require('tailwindcss'),
-    process.env.NODE_ENV === 'production' ? require('autoprefixer') : null,
-    process.env.NODE_ENV === 'production'
-      ? cssnano({
-          preset: 'default'
-        })
-      : null
+    require('autoprefixer'),
+    cssnano({
+      preset: 'default'
+    })
   ]
 };
